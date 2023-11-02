26aca2b7b006d7c8935844e02d8d8fd98eb86321
Felipe4Jaramillo <115907109+Felipe4Jaramillo@users.noreply.github.com>
2023-11-02 11:24:06 -0500

    First commit

---
+++ source/MainComponent.cpp
@@ -0,0 +1,223 @@
+#include "MainComponent.h"
+
+//==============================================================================
+MainComponent::MainComponent() : state(Stopped)
+{
+    //Metodo de juce para hacer visible un componenete
+    addAndMakeVisible(&openButton); //el & indica a un objeto o variable especifica
+    //Texto del boton
+    openButton.setButtonText("open...");
+
+    //Funcion lamda
+    //PAsarle una funcion a otra funcion
+    //Sintaxis
+    //[Donde esta la funcion] {la funcion]
+    openButton.onClick = [this] {openButtonClick(); };
+
+    //Metodo de juce para hacer visible un componenete
+    addAndMakeVisible(&playButton); //el & indica a un objeto o variable especifica
+ 
+    playButton.setButtonText("Play");
+
+    playButton.onClick = [this] {playButtonClick(); };
+
+    playButton.setColour(juce::TextButton::ColourIds::buttonColourId, juce::Colours::green);
+
+    playButton.setEnabled(false);
+
+    //Metodo de juce para hacer visible un componenete
+    addAndMakeVisible(&stopButton); //el & indica a un objeto o variable especifica
+
+    stopButton.setButtonText("Stop");
+
+    stopButton.onClick = [this] {stopButtonClick(); };
+
+    stopButton.setColour(juce::TextButton::ColourIds::buttonColourId, juce::Colours::red);
+
+
+    stopButton.setEnabled(false);
+
+    addAndMakeVisible(&granSlider);
+
+    granSlider.onValueChange = [this] {granKnob(); };
+
+    granSlider.setRange(minGranTime, maxGranTime);
+
+    //granSlider.addListener(this);
+
+    granSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag);
+    granSlider.setColour(juce::Slider::ColourIds::rotarySliderFillColourId, juce::Colours::black);
+    granSlider.setColour(juce::Slider::ColourIds::rotarySliderOutlineColourId, juce::Colours::white);
+
+    granSlider.isHorizontal();
+
+    granSlider.setEnabled(false);
+
+    //Que formatos vamos a reproducir
+    formatManager.registerBasicFormats();
+
+    transportSource.addChangeListener(this); 
+
+
+
+
+    // Make sure you set the size of the component after
+    // you add any child components.
+    setSize (800, 600);
+
+    // Some platforms require permissions to open input channels so request that here
+    if (juce::RuntimePermissions::isRequired (juce::RuntimePermissions::recordAudio)
+        && ! juce::RuntimePermissions::isGranted (juce::RuntimePermissions::recordAudio))
+    {
+        juce::RuntimePermissions::request (juce::RuntimePermissions::recordAudio,
+                                           [&] (bool granted) { setAudioChannels (granted ? 2 : 0, 2); });
+    }
+    else
+    {
+        // Specify the number of input and output channels that we want to open
+        setAudioChannels (2, 2);
+    }
+}
+
+MainComponent::~MainComponent()
+{
+    // This shuts down the audio device and clears the audio source.
+    shutdownAudio();
+}
+
+void MainComponent::changeState(TransportState newState) 
+{
+    //Revisa que no repitamos el mismo estado
+
+    if (state != newState) 
+    {
+        state = newState;
+
+        switch (state)
+        {
+        case Stopped:
+            stopButton.setEnabled(false);
+            playButton.setEnabled(true);
+            //Este setPosition determina donde queda luego de haber parado
+            transportSource.setPosition(startPosition);
+            break;
+
+        case Starting:         
+            playButton.setEnabled(false);
+            granSlider.setEnabled(true);
+            transportSource.start();
+            break;
+
+        case Playing:
+            stopButton.setEnabled(true);
+            break;
+
+        case Stopping:
+            transportSource.stop();
+            granSlider.setEnabled(false);
+            break;
+        }
+    }
+}
+
+void MainComponent::openButtonClick()
+{
+    //Abrir ventana para seleccionar archivo
+    //Buscar un archivo wav
+    //Cargar el archivo wav
+
+    //En la ventana de busqueda solo aparecen archivos wav
+    chooser = std::make_unique<juce::FileChooser>("Seleccione un archivo wav", juce::File{}, "*.wav");
+
+    auto chooserFlags = juce::FileBrowserComponent::openMode | juce::FileBrowserComponent::canSelectFiles;
+
+
+    //Se escogio el archivo
+    chooser->launchAsync(chooserFlags, [this](const juce::FileChooser& fc)
+    {
+
+            //Crear un objeto interno de juce para el archivo
+            auto file = fc.getResult();
+
+            //Si ese objeto no es tipo file
+            if (file != juce::File{})                                                
+            {
+                //VAmos a acomodarlo para JUCE
+                auto* reader = formatManager.createReaderFor(file);                 
+
+                //Si no existe un archivo, entonces el nuevo archivo va a reproduccion
+                if (reader != nullptr)
+                {
+                    //Enviamos el archivo a reproduccion en transportSource
+                    auto newSource = std::make_unique<juce::AudioFormatReaderSource>(reader, true);
+                    newSource -> setLooping(true);                                        
+                   
+                    transportSource.setSource(newSource.get(), 0, nullptr, reader->sampleRate);       
+                    playButton.setEnabled(true);
+                    //Este setPosition determina la posición inicial
+                    transportSource.setPosition(startPosition);
+                    readerSource.reset(newSource.release());                                          
+                }
+            }
+    });
+}
+
+void MainComponent::playButtonClick()
+{
+    changeState(Starting);
+}
+
+void MainComponent::stopButtonClick()
+{
+    changeState(Stopping);
+}
+
+void MainComponent::granKnob()
+{
+    granTime = granTime + minGranTime;
+}
+//==============================================================================
+void MainComponent::prepareToPlay (int samplesPerBlockExpected, double sampleRate)
+{
+    transportSource.prepareToPlay(samplesPerBlockExpected, sampleRate);
+}
+
+void MainComponent::getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill)
+{
+
+
+    //Si el archivo no es valido vamos a pasar silencio
+    if (readerSource.get() == nullptr)
+    {
+        bufferToFill.clearActiveBufferRegion();
+
+        return;
+    }
+
+    if (transportSource.getCurrentPosition() > (startPosition + granTime))
+    {
+        transportSource.setPosition(startPosition);
+    }
+
+    //de lo contrario vamos a meter el trasporSource (archivo wav) en el buffer
+    transportSource.getNextAudioBlock(bufferToFill);
+}
+
+
+//==============================================================================
+void MainComponent::paint (juce::Graphics& g)
+{
+    // (Our component is opaque, so we must completely fill the background with a solid colour)
+    g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
+
+    // You can add your drawing code here!
+}
+
+void MainComponent::resized()
+{
+    openButton.setBounds(20, 20, getWidth() - 50, getHeight() / 4);
+    playButton.setBounds(20, openButton.getBottom(), getWidth() - 50, getHeight() / 4);
+    stopButton.setBounds(20, playButton.getBottom(), getWidth() - 50, getHeight() / 4);
+
+    granSlider.setBounds(20, stopButton.getBottom(), getWidth() - 50, getHeight() / 6);
+}
---
+++ source/MainComponent.h
@@ -0,0 +1,112 @@
+#pragma once
+
+#include <JuceHeader.h>
+
+//==============================================================================
+/*
+    This component lives inside our window, and this is where you should put all
+    your controls and content.
+*/
+class MainComponent  : public juce::AudioAppComponent, juce::ChangeListener//, juce::Slider::Listener
+
+{
+    enum TransportState
+    {
+        Stopped,
+        Starting,
+        Playing,
+        Stopping
+    };
+
+
+public:
+    //==============================================================================
+    MainComponent();
+    ~MainComponent() override;
+
+    //==============================================================================
+    void prepareToPlay (int samplesPerBlockExpected, double sampleRate) override;
+    void getNextAudioBlock (const juce::AudioSourceChannelInfo& bufferToFill) override;
+
+    //==============================================================================
+    void paint (juce::Graphics& g) override;
+    void resized() override;
+
+    //Override: hacemos que el metodo existente pero queremos que haga lo que nosotros queramos
+    void changeListenerCallback(juce::ChangeBroadcaster* source) override
+    {
+        //Asegurarce que recibimos el tipo de archivo correcto
+        if (source == &transportSource)
+        {
+            if (transportSource.isPlaying())
+            {
+                changeState(Playing);
+            }
+            else
+            {
+                changeState(Stopped);
+            }
+        }
+    }
+
+    void openButtonClick();
+    void playButtonClick();
+    void stopButtonClick();
+
+    void granKnob();
+
+   
+   /* void sliderValueChanged(juce::Slider* slider) override
+    {
+        if (slider == &granSlider)
+        {
+            minGranTime++;
+        }
+    }
+    */
+   
+    void releaseResources() override
+    {
+        transportSource.releaseResources();
+    }
+
+    void changeState(TransportState newState);
+
+    juce::AudioFormatManager formatManager;
+
+    //Pointer: direccion en memoria
+    //unique_ptr: direccion en memoria que se administra sola
+    //AudioFormatReaderSource objeto que lee archivo de audio
+    std::unique_ptr<juce::AudioFormatReaderSource> readerSource;
+
+    //Identifica la ubicacion de reproduccion
+    juce::AudioTransportSource transportSource;
+
+
+
+private:
+
+    TransportState state;
+
+    std::unique_ptr<juce::FileChooser> chooser;
+
+    //Boton para abrir el archivo de audio
+    juce::TextButton openButton;
+    //boton de paly
+    juce::TextButton playButton;
+    //boton de stop
+    juce::TextButton stopButton;
+
+    juce::Slider granSlider;
+
+    double totalDuration;
+    float startPosition = 40.0;
+    double minGranTime = 0.02;
+    double maxGranTime = 3.0;
+    double granTime = 0.02;
+    //==============================================================================
+    // Your private member variables go here...
+
+
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (MainComponent)
+};
